07ffdaf85c5bfef8c639c47e8b814f0f68dd870d
Jordan Marta <jordan.smta@gmail.com>
2020-11-29 09:51:15 -0300

    Alterações Backend

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('mysql://root:12102013@localhost:3306/pitu');
+const sequelize = new Sequelize('mysql://root:password@localhost:3306/pitu');
 
 export default sequelize;
